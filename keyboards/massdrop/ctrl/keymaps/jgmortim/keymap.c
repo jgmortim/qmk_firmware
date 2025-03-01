97efe6cc3db2fee464ae60d5d15687b4049644a2
jgmortim <jgmortim@mtu.edu>
2025-03-01 01:02:05 -0500

    Add Linux support

--- keyboards/massdrop/ctrl/keymaps/jgmortim/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/jgmortim/keymap.c
@@ -23,9 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define COPPER {12, 255, 100}
 #define CPPR_BRT {12, 255, 225}
 #define CYAN {HSV_CYAN}
-#define ______ {HSV_BLACK} // 5 underscores instead of the 6 used by the KC_TRNS alias.
+#define WIN_IND {HSV_BLUE}  // Windows mode indicator LED color.
+#define LNX_IND {HSV_GREEN} // Linux mode indicator LED color.
+#define ______ {HSV_OFF}    // 5 underscores instead of the 6 used by the KC_TRNS alias.
 
-#define RGB_TIME_OUT 300  // 300 seconds (5 minutes).
+#define RGB_TIME_OUT 300       // 300 seconds (5 minutes).
+#define OS_MODE_IND_LED 77     // Index of the OS mode indicator LED (77 is Win key).
+#define OS_MODE_IND_TIME_OUT 3 // 3 second timeout for the OS mode indicator LED.
 
 extern rgb_config_t rgb_matrix_config;
 
@@ -34,6 +38,8 @@ bool rgb_time_out_enable;               // Idle LED toggle enable. If false then
 bool rgb_time_out_user_value;           // This holds the toggle value set by user with ROUT_TG. It's necessary as RGB_TOG changes timeout enable.
 uint16_t rgb_time_out_seconds;          // Idle LED timeout value, in seconds not milliseconds
 led_flags_t rgb_time_out_saved_flag;    // Store LED flag before timeout so it can be restored when LED is turned on again.
+uint8_t os_mode;                        // Stores the current OS mode.
+bool os_mode_led_flag;                  // Current OS indicator LED state flag. If false, then the LED is off.
 
 enum tapdance_keycodes {
     TD_ALT_SL = 0, // Tap dance key to switch to Spanish layer
@@ -66,12 +72,21 @@ enum custom_keycodes {
     U_ACUTE,               // U with acute accent
     ENE,                   // N with tilde accent
     INV_EXC,               // Inverted exclamation point
-    INV_QUS                // Inverted question mark
+    INV_QUS,               // Inverted question mark
+    OS_TOG                 // Toggle OS mode between Windows and Linux
+};
+
+enum os {
+    WINDOWS = 0,
+    LINUX
 };
 
 static uint16_t idle_timer;             // Idle LED timeout timer
 static uint16_t idle_second_counter;    // Idle LED seconds counter, counts seconds not milliseconds
 static uint8_t key_event_counter;       // This counter is used to check if any keys are being held
+
+static uint16_t os_ind_led_timer;          // OS toggle LED timeout timer
+static uint16_t os_ind_led_second_counter; // OS toggle LED seconds counter, counts seconds not milliseconds
 
 /* Associate tap dance keys with their functionality */
 tap_dance_action_t tap_dance_actions[] = {
@@ -94,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______, _______, _______, _______, _______, U_T_AUTO,U_T_AGCR,_______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_VOLD,
         _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______,  _______, _______, _______, MD_BOOT, NK_TOGG, _______, _______, _______, _______, _______,                            _______,
-        _______, _______,  _______,                   _______,                            _______, _______, _______, _______,          _______, _______, _______
+        _______, OS_TOG,   _______,                   _______,                            _______, _______, _______, _______,          _______, _______, _______
     ),
     [_NL] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_KP_7, KC_KP_8, KC_KP_9,
@@ -162,6 +177,8 @@ void matrix_init_user(void) {
     rgb_time_out_enable = true;                         // Enable RGB timeout by default.
     rgb_enabled_flag = true;                            // Initially, keyboard RGB is enabled.
     rgb_time_out_saved_flag = rgb_matrix_get_flags();   // Save RGB matrix state for when keyboard comes back from ide.
+    os_mode = WINDOWS;                                  // Default to Windows mode.
+    os_mode_led_flag = false;                           // Default OS indicator LED to off.
 };
 
 /* Runs just one time after everything else has initialized. */
@@ -188,9 +205,21 @@ void matrix_scan_user(void) {
             idle_second_counter = 0;
         }
     }
+
+    if(os_mode_led_flag) {
+        if (timer_elapsed(os_ind_led_timer) > MILLISECONDS_IN_SECOND) {
+            os_ind_led_second_counter++;
+            os_ind_led_timer = timer_read();
+        }
+
+        if (os_ind_led_second_counter >= OS_MODE_IND_TIME_OUT) {
+            os_mode_led_flag = false;
+            os_ind_led_second_counter = 0;
+        }
+    }
 };
 
-/* Executes the given alt code. */
+/* Executes the given alt code (Windows only). */
 void send_alt_code(char code[]) {
     uint8_t mod_state = get_mods(); // Gets the current mods
     unregister_mods(mod_state);     // Turns all active mods off
@@ -215,7 +244,19 @@ void send_alt_code(char code[]) {
     register_mods(mod_state); // Restores the mods to their original state
 }
 
-/* Enters the given command into the Windows Run dialog. */
+/* Inserts the given Unicode character via Unicode-entry mode (Linux only). */
+void send_unicode(const char *code) {
+    uint8_t mod_state = get_mods(); // Gets the current mods
+    unregister_mods(mod_state);     // Turns all active mods off
+
+    SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_RSFT) "u" SS_UP(X_RSFT) SS_UP(X_LCTL)); // Ctrl + Shift  + U,
+    SEND_STRING(code);                                                            // type in the code,
+    SEND_STRING(SS_TAP(X_ENT));                                                   // and press enter.
+
+    register_mods(mod_state); // Restores the mods to their original state
+}
+
+/* Enters the given command into the Windows Run dialog (Windows only). */
 void windows_run(const char *command) {
     SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(50)); // Open the run dialog,
     SEND_STRING(command);                                                // type in the command,
@@ -248,84 +289,117 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     switch (keycode) {
+        /* Toggle OS mode between Windows and Linux */
+        case OS_TOG:
+            if (record->event.pressed) {
+                os_mode ^= (WINDOWS | LINUX);
+                os_mode_led_flag = true;
+                os_ind_led_second_counter = 0;
+                return false;
+            }
+            return true;
         /* Change Win + c to be Calculator instead of Cortana */
         case KC_C:
-            if (record->event.pressed && get_mods() == MOD_BIT(KC_LGUI)) {
+            if (record->event.pressed && get_mods() == MOD_BIT(KC_LGUI) && os_mode == WINDOWS) {
                 windows_run("CALC");
                 return false;
             }
             return true;
         /* Change Win + s to be Snipping Tool instead of search */
         case KC_S:
-            if (record->event.pressed && get_mods() == MOD_BIT(KC_LGUI)) {
+            if (record->event.pressed && get_mods() == MOD_BIT(KC_LGUI) && os_mode == WINDOWS) {
                 windows_run("SnippingTool");
                 return false;
             }
             return true;
         /* Spanish Letters and Punctuation */
-        case A_ACUTE:
+        case A_ACUTE: // A with acute accent
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0193")  // Uppercase A with acute accent: ALT + 0193
-                    : send_alt_code("0225"); // Lowercase a with acute accent: ALT + 0225
+                if (os_mode == WINDOWS) {
+                    send_alt_code(MODS_SHIFT ? "0193" : "0225");
+                } else {
+                    send_unicode(MODS_SHIFT ? "00C1" : "00E1");
+                }
+                layer_off(3);
             }
-            layer_off(3);
             return false;
-        case E_ACUTE:
+        case E_ACUTE: // E with acute accent
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0201")  // Uppercase E with acute accent: ALT + 0201
-                    : send_alt_code("0233"); // Lowercase e with acute accent: ALT + 0233
+                if (os_mode == WINDOWS) {
+                    send_alt_code(MODS_SHIFT ? "0201" : "0233");
+                } else {
+                    send_unicode(MODS_SHIFT ? "00C9" : "00E9");
+                }
+                layer_off(3);
             }
-            layer_off(3);
             return false;
-        case I_ACUTE:
+        case I_ACUTE: // I with acute accent
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0205")  // Uppercase I with acute accent: ALT + 0205
-                    : send_alt_code("0237"); // Lowercase i with acute accent: ALT + 0237
+                if (os_mode == WINDOWS) {
+                    send_alt_code(MODS_SHIFT ? "0205" : "0237");
+                } else {
+                    send_unicode(MODS_SHIFT ? "00CD" : "00ED");
+                }
+                layer_off(3);
             }
-            layer_off(3);
             return false;
-        case O_ACUTE:
+        case O_ACUTE: // O with acute accent
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0211")  // Uppercase O with acute accent: ALT + 0211
-                    : send_alt_code("0243"); // Lowercase o with acute accent: ALT + 0243
+                if (os_mode == WINDOWS) {
+                    send_alt_code(MODS_SHIFT ? "0211" : "0243");
+                } else {
+                    send_unicode(MODS_SHIFT ? "00D3" : "00F3");
+                }
+                layer_off(3);
             }
-            layer_off(3);
             return false;
-        case U_ACUTE:
+        case U_ACUTE: // U with acute accent
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0218")  // Uppercase U with acute accent: ALT + 0218
-                    : send_alt_code("0250"); // Lowercase u with acute accent: ALT + 0250
+                if (os_mode == WINDOWS) {
+                    send_alt_code(MODS_SHIFT ? "0218" : "0250");
+                } else {
+                    send_unicode(MODS_SHIFT ? "00DA" : "00FA");
+                }
+                layer_off(3);
             }
-            layer_off(3);
             return false;
-        case ENE:
+        case ENE: // N with tilde accent
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0209")  // Uppercase N with tilde accent: ALT + 0209
-                    : send_alt_code("0241"); // Lowercase n with tilde accent: ALT + 0241
+                if (os_mode == WINDOWS) {
+                    send_alt_code(MODS_SHIFT ? "0209" : "0241");
+                } else {
+                    send_unicode(MODS_SHIFT ? "00D1" : "00F1");
+                }
+                layer_off(3);
             }
-            layer_off(3);
             return false;
         case INV_EXC:
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0161")  // Inverted exclamation point: ALT + 0161
-                    : SEND_STRING("1");
+                if (MODS_SHIFT) { // Inverted exclamation point
+                    if (os_mode == WINDOWS) {
+                        send_alt_code("0161");
+                    } else {
+                        send_unicode("00A1");
+                    }
+                    layer_off(3);
+                } else {
+                    SEND_STRING("1");
+                }
             }
-            layer_off(3);
             return false;
         case INV_QUS:
             if (record->event.pressed) {
-                MODS_SHIFT
-                    ? send_alt_code("0191")  // Inverted question mark: ALT + 0191
-                    : SEND_STRING("/");
+                if (MODS_SHIFT) { // Inverted question mark
+                    if (os_mode == WINDOWS) {
+                        send_alt_code("0191");
+                    } else {
+                        send_unicode("00BF");
+                    }
+                    layer_off(3);
+                } else {
+                    SEND_STRING("/");
+                }
             }
-            layer_off(3);
             return false;
         /* Defaults */
         case U_T_AUTO:
@@ -372,8 +446,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+/* Runs everytime a layer change happens */
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        /* When turning on the numpad, make sure NUM LOCK is on */
+        case _NL:
+            bool num_lock = host_keyboard_led_state().num_lock;
+            if (!num_lock) { // If NUM LOCK isn't on, turn it on.
+                register_code(KC_NUM);
+                unregister_code(KC_NUM);
+            }
+            break;
+        /* No special logic for the other layers */
+        default:
+            break;
+    }
+  return state;
+}
+
 void set_layer_color(int layer) {
     for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+        /* Special Indicator Logic */
+        if (i == OS_MODE_IND_LED && (os_mode_led_flag || layer == _FL)) {
+            HSV win_hsv = WIN_IND;
+            HSV lnx_hsv = LNX_IND;
+
+            RGB rgb = hsv_to_rgb(os_mode == WINDOWS ? win_hsv : lnx_hsv);
+            float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+            rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
+            continue;
+        }
+        /* Normal Logic */
         HSV hsv = {
             .h = pgm_read_byte(&ledmap[layer][i][0]),
             .s = pgm_read_byte(&ledmap[layer][i][1]),
